ce12fc79d834628f8c09400e3dd2575bc9b7a65f
Demis Bellot <demis.bellot@gmail.com>
2014-09-16 15:59:35 -0400

    Add RequiresRole page

---
+++ src/Mvc/Mvc/Controllers/RequiresRoleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Mvc.ServiceInterface;
+using ServiceStack;
+using ServiceStack.Mvc;
+
+namespace Mvc.Controllers
+{
+    [RequiredRole("TheRole")]
+    public class RequiresRoleController : ServiceStackController 
+    {
+        public ActionResult Index()
+        {
+            var session = SessionAs<CustomUserSession>();
+            return View(session);
+        }
+    }
+}
