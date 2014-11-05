ff807e7058e2756a2f6cbb40a2716fba6a7fbd9d
Demis Bellot <demis.bellot@gmail.com>
2014-11-04 19:30:25 -0500

    Add RabbitMqTest project

---
+++ src/RabbitMqTest/RabbitMqTest.Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Messaging;
+using ServiceStack.RabbitMq;
+using ServiceStack.Text;
+
+namespace RabbitMQTest.Client
+{
+    public class HelloRabbit
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloRabbitResponse
+    {
+        public string Result { get; set; }
+    }
+
+    class Program
+    {
+        public static IMessageService CreateMqServer(int retryCount = 1)
+        {
+            return new RabbitMqServer { RetryCount = retryCount };
+        }
+
+        static void Main(string[] args)
+        {
+            typeof(Program).Namespace.Print();
+
+            using (var mqServer = CreateMqServer())
+            using (var mqClient = mqServer.CreateMessageQueueClient())
+            {
+                var replyToMq = mqClient.GetTempQueueName();
+                mqClient.Publish(new Message<HelloRabbit>(new HelloRabbit { Name = "World" })
+                {
+                    ReplyTo = replyToMq
+                });
+
+                IMessage<HelloRabbitResponse> responseMsg = mqClient.Get<HelloRabbitResponse>(replyToMq);
+                mqClient.Ack(responseMsg);
+
+
+                responseMsg.GetBody().PrintDump();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ src/RabbitMqTest/RabbitMqTest.Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Messaging;
+using ServiceStack.RabbitMq;
+using ServiceStack.Text;
+
+namespace RabbitMqTest.Server
+{
+    public class HelloRabbit
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloRabbitResponse
+    {
+        public string Result { get; set; }
+    }
+
+    class Program
+    {
+        public static IMessageService CreateMqServer(int retryCount = 1)
+        {
+            return new RabbitMqServer { RetryCount = retryCount };
+        }
+
+        static void Main(string[] args)
+        {
+            using (var mqServer = CreateMqServer())
+            {
+                mqServer.RegisterHandler<HelloRabbit>(m =>
+                    new HelloRabbitResponse { Result = "Hello, {0}!".Fmt(m.GetBody().Name) });
+
+                mqServer.Start();
+
+                typeof(Program).Namespace.Print();
+                Console.ReadKey();
+            }
+        }
+    }
+}
