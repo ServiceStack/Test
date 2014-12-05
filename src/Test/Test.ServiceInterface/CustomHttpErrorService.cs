50a93630f843ce2c43cdd5f8523476cb1e38a3e2
Demis Bellot <demis.bellot@gmail.com>
2014-12-05 18:00:10 -0500

    Add CustomHttpError Service

---
+++ src/Test/Test.ServiceInterface/CustomHttpErrorService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using Test.ServiceModel;
+
+namespace Test.ServiceInterface
+{
+    public class CustomHttpErrorService : Service
+    {
+        public object Any(CustomHttpError request)
+        {
+            throw new HttpError(request.StatusCode, request.StatusDescription);
+        }
+    }
+}
---
+++ src/Test/Test.ServiceModel/CustomHttpError.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack;
+
+namespace Test.ServiceModel
+{
+    public class CustomHttpError
+    {
+        public int StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+    }
+
+    public class CustomHttpErrorResponse
+    {
+        public string Custom { get; set; }
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
