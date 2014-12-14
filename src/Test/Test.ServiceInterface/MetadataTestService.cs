9d89723f8a6ea1517d958177837c129601f8a1a2
Demis Bellot <demis.bellot@gmail.com>
2014-12-14 12:54:33 -0500

    Add MetadataTest Service

---
+++ src/Test/Test.ServiceInterface/MetadataTestService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using ServiceStack;
+using Test.ServiceModel;
+
+namespace Test.ServiceInterface
+{
+    public class MetadataTestService : Service
+    {
+        public object Any(MetadataTest request)
+        {
+            return new MetadataTestResponse
+            {
+                Id = request.Id,
+                Results = new List<MetadataTestChild>
+                {
+                    new MetadataTestChild
+                    {
+                        Name = "foo",
+                        Results = new List<MetadataTestNestedChild>
+                        {
+                            new MetadataTestNestedChild
+                            {
+                                Name = "bar",
+                            },
+                        }
+                    }
+                }
+            };
+        }
+
+        public object Any(GetExample request)
+        {
+            return new GetExampleResponse
+            {
+                MenuExample1 = new MenuExample
+                {
+                    MenuItemExample1 = new MenuItemExample { Name1 = "foo" }
+                }
+            };
+        }
+    }
+}
---
+++ src/Test/Test.ServiceModel/MetadataTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using ServiceStack;
+
+namespace Test.ServiceModel
+{
+    public class MetadataTest : IReturn<MetadataTestResponse>
+    {
+        public int Id { get; set; }
+    }
+
+    public class MetadataTestResponse
+    {
+        public int Id { get; set; }
+        public List<MetadataTestChild> Results { get; set; }
+    }
+
+    public class MetadataTestChild
+    {
+        public string Name { get; set; }
+
+        public List<MetadataTestNestedChild> Results { get; set; }
+    }
+
+    public class MetadataTestNestedChild
+    {
+        public string Name { get; set; }
+    }
+
+
+    [Route("/example", "GET", Summary = @"")]
+    [DataContract]
+    public class GetExample : IReturn<GetExampleResponse>
+    {
+    }
+
+    [DataContract]
+    public class GetExampleResponse : IHasResponseStatus
+    {
+        [DataMember(Order = 1, IsRequired = false)]
+        public ResponseStatus ResponseStatus { get; set; }
+
+        [ApiMember]
+        [DataMember(Order = 2, IsRequired = false)]
+        public MenuExample MenuExample1 { get; set; }
+    }
+
+    [DataContract]
+    public class MenuExample
+    {
+        [ApiMember]
+        [DataMember(Order = 1, IsRequired = false)]
+        public MenuItemExample MenuItemExample1 { get; set; }
+    }
+
+    //[DataContract]
+    public class MenuItemExample
+    {
+        [ApiMember]
+        [DataMember(Order = 1, IsRequired = false)]
+        public string Name1 { get; set; }
+
+        public MenuItemExampleItem MenuItemExampleItem { get; set; }
+    }
+
+    public class MenuItemExampleItem
+    {
+        [ApiMember]
+        [DataMember(Order = 1, IsRequired = false)]
+        public string Name1 { get; set; }
+    }
+}
