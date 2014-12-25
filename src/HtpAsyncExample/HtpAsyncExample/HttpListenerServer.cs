fd7e8e236c8056b6d6c9f970f91dba5b7c4af315
Demis Bellot <demis.bellot@gmail.com>
2014-12-25 17:49:38 -0500

    Add HttpAsync Example test project

---
+++ src/HtpAsyncExample/HtpAsyncExample/HttpListenerServer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HtpAsyncExample
+{
+    public class HttpListenerServer
+    {
+        protected HttpListener Listener;
+        protected bool IsStarted = false;
+
+        private readonly AutoResetEvent ListenForNextRequest = new AutoResetEvent(false);
+
+        public Func<HttpListenerContext, Task> OnRequest;
+
+        public HttpListenerServer Start(string urlBase)
+        {
+            Start(urlBase, Listen);
+            return this;
+        }
+
+        protected void Start(string urlBase, WaitCallback listenCallback)
+        {
+            // *** Already running - just leave it in place
+            if (this.IsStarted)
+                return;
+
+            if (this.Listener == null)
+                Listener = new HttpListener();
+
+            Listener.Prefixes.Add(urlBase);
+
+            Listener.Start();
+            IsStarted = true;
+
+            ThreadPool.QueueUserWorkItem(listenCallback);
+        }
+
+        private bool IsListening
+        {
+            get { return this.IsStarted && this.Listener != null && this.Listener.IsListening; }
+        }
+
+        protected virtual void Listen(object state)
+        {
+            while (IsListening)
+            {
+                if (Listener == null) return;
+                try
+                {
+                    Listener.BeginGetContext(ListenerCallback, Listener);
+                    ListenForNextRequest.WaitOne();
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+                if (Listener == null) return;
+            }
+        }
+
+        private void ListenerCallback(IAsyncResult asyncResult)
+        {
+            var listener = asyncResult.AsyncState as HttpListener;
+            HttpListenerContext context = null;
+
+            if (listener == null) return;
+
+            try
+            {
+                context = listener.EndGetContext(asyncResult);
+            }
+            catch (Exception ignore) {}
+            finally
+            {
+                ListenForNextRequest.Set();
+            }
+
+            if (context == null) return;
+
+
+            InitTask(context);
+        }
+
+        public virtual void InitTask(HttpListenerContext context)
+        {
+            try
+            {
+                var task = this.OnRequest(context);
+                task.ContinueWith(x => HandleError(x.Exception, context), TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.AttachedToParent);
+
+                if (task.Status == TaskStatus.Created)
+                {
+                    task.RunSynchronously();
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError(ex, context);
+            }
+        }
+
+        public static void HandleError(Exception ex, HttpListenerContext context)
+        {
+            Console.WriteLine("ERROR: " + ex);
+        }
+    }
+}
---
+++ src/HtpAsyncExample/HtpAsyncExample/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using ServiceStack;
+
+namespace HtpAsyncExample
+{
+    class Program
+    {
+        const string Url = "http://localhost:10000/";
+        const int NoOfClients = 5;
+
+        static void Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            var activeConnections = new List<HttpListenerContext>();
+
+            int counter = 0;
+            var clientId = 0;
+            var httpListener = new HttpListenerServer
+            {
+                OnRequest = ctx =>
+                {
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(string.Format("id: {0}\ndata: cmd.onConnect {{\"id\":\"CLIENT_{1}\"}}\n\n", ++counter, ++clientId));
+                    ctx.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                    activeConnections.Add(ctx);
+                    return tcs.Task;
+                }
+            }.Start(Url);
+
+            Timer timer = null;
+            timer = new Timer(_ =>
+                {
+                    var bytes = System.Text.Encoding.UTF8.GetBytes(string.Format("id: {0}\ndata: cmd.onMessage MESSAGE {0}\n\n", ++counter));
+                    foreach (var ctx in activeConnections)
+                    {
+                        ctx.Response.OutputStream.Write(bytes, 0, bytes.Length);
+                    }
+                    timer.Change((int) TimeSpan.FromSeconds(1).TotalMilliseconds, Timeout.Infinite);
+                },
+                null, 
+                (int)TimeSpan.FromSeconds(1).TotalMilliseconds, Timeout.Infinite);
+
+            //CreateBasicHttpClients(NoOfClients);
+            CreateServerEventsClient(NoOfClients);
+
+            Console.ReadLine();
+        }
+
+        private static void CreateServerEventsClient(int noOfClients)
+        {
+            var clients = new List<ServerEventsClient>();
+            for (int i = 0; i < noOfClients; i++)
+            {
+                var clientId = i;
+                var client = new ServerEventsClient(Url, "HOME") {
+                    OnMessage = e => Console.WriteLine("ServerEventsClient #{0} {1}", clientId, e.Data)
+                }.Start();
+
+                clients.Add(client);
+            }
+        }
+
+        private static void CreateBasicHttpClients(int noOfClients)
+        {
+            var clients = new List<HttpWebRequest>();
+            for (int i = 0; i < noOfClients; i++)
+            {
+                var httpReq = (HttpWebRequest) WebRequest.Create(Url);
+                clients.Add(httpReq);
+
+                var httpRes = (HttpWebResponse) httpReq.GetResponse();
+                var stream = httpRes.GetResponseStream();
+
+                ProcessBasicResponse(stream, i);
+            }
+        }
+
+        private static void ProcessBasicResponse(Stream stream, int clientId)
+        {
+            var buffer = new byte[1024];
+
+            var task = stream.ReadAsync(buffer, 0, buffer.Length);
+            task.ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    Console.WriteLine("ERROR CLIENT: #{0} {1}", clientId, t.Exception);
+                    return;
+                }
+
+                var length = t.Result;
+
+                if (length > 0)
+                {
+                    var message = System.Text.Encoding.UTF8.GetString(buffer, 0, length);
+                    Console.WriteLine("HttpWebRequest: #{0} {1}", clientId, message);
+
+                    ProcessBasicResponse(stream, clientId);
+                }
+            });
+        }
+    }
+}
