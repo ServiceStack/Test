c25dac0450c51f58f840d0b682d1123ab8790db5
Demis Bellot <demis.bellot@gmail.com>
2014-12-25 17:50:22 -0500

    Add Server Events Multi Channel Test Project

---
+++ src/ServerEventsMultiChannelTest/MultiChannelClient/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace MultiChannelServer
+{
+    [Route("/channels/{Channel}/chat")]
+    public class PostChatToChannel : IReturn<ChatMessage>
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    public class ChatMessage
+    {
+        public long Id { get; set; }
+        public string FromUserId { get; set; }
+        public string FromName { get; set; }
+        public string DisplayName { get; set; }
+        public string Message { get; set; }
+        public string UserAuthId { get; set; }
+        public bool Private { get; set; }
+    }
+
+    [Route("/channels/{Channel}/raw")]
+    public class PostRawToChannel : IReturnVoid
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    [Route("/channels/{Channel}/object")]
+    public class PostObjectToChannel
+    {
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Selector { get; set; }
+
+        public CustomType CustomType { get; set; }
+        public SetterType SetterType { get; set; }
+    }
+
+    public class CustomType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SetterType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static class ServerClientExtensions
+    {
+        public static void PostChat(this ServerEventsClient client,
+            string message, string channel = null)
+        {
+            client.ServiceClient.PostChat(client.SubscriptionId, message, channel);
+        }
+
+        public static void PostChat(this IServiceClient client, string subsciptionId,
+            string message, string channel = null)
+        {
+            client.Post(new PostChatToChannel
+            {
+                From = subsciptionId,
+                Message = message,
+                Channel = channel ?? "*",
+                Selector = "cmd.chat",
+            });
+        }
+    }
+
+    class Program
+    {
+        private static class Config
+        {
+            public static string AbsoluteBaseUri = "http://macbook:10000/";
+        }
+
+        private static ServerEventsClient CreateServerEventsClient(params string[] channels)
+        {
+            return new ServerEventsClient(Config.AbsoluteBaseUri, channels);
+        }
+
+        static void Main(string[] args)
+        {
+            Run();
+            Console.ReadLine();
+        }
+
+        static async Task Run()
+        {
+            using (var clientA = CreateServerEventsClient("A"))
+            using (var clientAB = CreateServerEventsClient("A", "B"))
+            using (var clientABC = CreateServerEventsClient("A", "B", "C"))
+            using (var clientABCD = CreateServerEventsClient("A", "B", "C", "D"))
+            {
+                var msgsA = new List<ServerEventMessage>();
+                var msgsAB = new List<ServerEventMessage>();
+                var msgsABC = new List<ServerEventMessage>();
+                var msgsABCD = new List<ServerEventMessage>();
+
+                clientA.OnMessage = msgsA.Add;
+                clientAB.OnMessage = msgsAB.Add;
+                clientABC.OnMessage = msgsABC.Add;
+                clientABCD.OnMessage = msgsABCD.Add;
+
+                await Task.WhenAll(
+                    clientA.Connect(),
+                    clientAB.Connect(),
+                    clientABC.Connect(),
+                    clientABCD.Connect()
+                );
+
+                "Publishing Msgs...".Print();
+                clientA.PostChat("#1 hello to A", channel: "A");
+                clientA.PostChat("#2 hello to B", channel: "B");
+                clientA.PostChat("#3 hello to C", channel: "C");
+                clientA.PostChat("#4 hello to D", channel: "D");
+
+                await Task.Delay(1000);
+
+                msgsA.PrintDump();
+                msgsAB.PrintDump();
+                msgsABC.PrintDump();
+                msgsABCD.PrintDump();
+
+                "msgsA: {0}".Print(msgsA.Count);
+                "msgsAB: {0}".Print(msgsAB.Count);
+                "msgsABC: {0}".Print(msgsABC.Count);
+                "msgsABCD: {0}".Print(msgsABCD.Count);
+            }
+        }
+    }
+}
---
+++ src/ServerEventsMultiChannelTest/MultiChannelServer/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+using Funq;
+using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Redis;
+using ServiceStack.Text;
+
+namespace MultiChannelServer
+{
+    public class ServerEventsService : Service
+    {
+        private static long msgId;
+
+        public IServerEvents ServerEvents { get; set; }
+
+        public object Any(PostChatToChannel request)
+        {
+            var sub = ServerEvents.GetSubscriptionInfo(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            var msg = new ChatMessage
+            {
+                Id = Interlocked.Increment(ref msgId),
+                FromUserId = sub.UserId,
+                FromName = sub.DisplayName,
+                Message = request.Message,
+            };
+
+            if (request.ToUserId != null)
+            {
+                msg.Private = true;
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, msg);
+                var toSubs = ServerEvents.GetSubscriptionInfosByUserId(request.ToUserId);
+                foreach (var toSub in toSubs)
+                {
+                    msg.Message = "@{0}: {1}".Fmt(toSub.DisplayName, msg.Message);
+                    ServerEvents.NotifySubscription(request.From, request.Selector, msg);
+                }
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, msg);
+            }
+
+            return msg;
+        }
+
+        public void Any(PostRawToChannel request)
+        {
+            var sub = ServerEvents.GetSubscriptionInfo(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            if (request.ToUserId != null)
+            {
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, request.Message);
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, request.Message);
+            }
+        }
+
+        public void Any(PostObjectToChannel request)
+        {
+            if (request.ToUserId != null)
+            {
+                if (request.CustomType != null)
+                    ServerEvents.NotifyUserId(request.ToUserId, request.Selector ?? Selector.Id<CustomType>(), request.CustomType);
+                if (request.SetterType != null)
+                    ServerEvents.NotifyUserId(request.ToUserId, request.Selector ?? Selector.Id<SetterType>(), request.SetterType);
+            }
+            else
+            {
+                if (request.CustomType != null)
+                    ServerEvents.NotifyChannel(request.Channel, request.Selector ?? Selector.Id<CustomType>(), request.CustomType);
+                if (request.SetterType != null)
+                    ServerEvents.NotifyChannel(request.Channel, request.Selector ?? Selector.Id<SetterType>(), request.SetterType);
+            }
+        }
+    }
+    public class ServerEventsAppHost : AppSelfHostBase
+    {
+        public ServerEventsAppHost()
+            : base(typeof(ServerEventsAppHost).Name, typeof(ServerEventsAppHost).Assembly) { }
+
+        public bool UseRedisServerEvents { get; set; }
+        public bool LimitToAuthenticatedUsers { get; set; }
+
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new ServerEventsFeature
+            {
+                HeartbeatInterval = TimeSpan.FromMilliseconds(30000),
+                LimitToAuthenticatedUsers = LimitToAuthenticatedUsers,
+            });
+
+            if (UseRedisServerEvents)
+            {
+                container.Register<IRedisClientsManager>(new PooledRedisClientManager());
+
+                container.Register<IServerEvents>(c =>
+                    new RedisServerEvents(c.Resolve<IRedisClientsManager>()));
+
+                container.Resolve<IServerEvents>().Start();
+            }
+
+            if (LimitToAuthenticatedUsers)
+            {
+                Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+                    new IAuthProvider[] {
+                        new CustomCredentialsAuthProvider(), 
+                    }));
+            }
+        }
+    }
+
+    public class CustomCredentialsAuthProvider : CredentialsAuthProvider
+    {
+        public override bool TryAuthenticate(IServiceBase authService, string userName, string password)
+        {
+            return userName == "user" && password == "pass";
+        }
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appHost = new ServerEventsAppHost()
+                .Init()
+                .Start("http://*:10000/");
+
+            "Started MultiChannelServer on http://macbook:10000/ ...".Print();
+            "Press Enter to Quit".Print();
+
+            Console.ReadLine();
+        }
+    }
+}
