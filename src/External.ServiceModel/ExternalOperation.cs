627fa98831b2b6102d0e46e7c6020fcfa0c74126
Demis Bellot <demis.bellot@gmail.com>
2015-01-19 19:11:29 -0500

    Add External types in an external .dll to repro missing types issue

---
+++ src/External.ServiceModel/ExternalOperation.cs
@@ -0,0 +1,80 @@
+﻿using ServiceStack;
+
+namespace External.ServiceModel
+{
+    public class ExternalOperation : IReturn<ExternalOperationResponse>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ExternalEnum ExternalEnum { get; set; }
+    }
+
+    public class ExternalOperationResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public enum ExternalEnum
+    {
+        Foo,
+        Bar,
+        Baz
+    }
+
+    public class ExternalOperation2 : IReturn<ExternalOperation2Response>
+    {
+        public int Id { get; set; }
+    }
+
+    public class ExternalOperation2Response
+    {
+        public ExternalType ExternalType { get; set; }
+    }
+
+    public class ExternalType
+    {
+        public ExternalEnum2 ExternalEnum2 { get; set; }
+    }
+
+    public enum ExternalEnum2
+    {
+        Uno,
+        Due,
+        Tre
+    }
+
+    public class ExternalOperation3
+    {
+        public int Id { get; set; }
+    }
+
+    public class ExternalReturnTypeResponse
+    {
+        public ExternalEnum3 ExternalEnum3 { get; set; }
+    }
+
+    public enum ExternalEnum3
+    {
+        Un,
+        Deux,
+        Trois
+    }
+
+    public class ExternalOperation4
+    {
+        public int Id { get; set; }
+    }
+
+    public class ExternalUnmatchedResponse
+    {
+        public ExternalEnum4 ExternalEnum4 { get; set; }
+    }
+
+    public enum ExternalEnum4
+    {
+        Uno,
+        Dos,
+        Tres
+    }
+
+}
---
+++ src/Test/Test.ServiceInterface/ExternalServices.cs
@@ -0,0 +1,40 @@
+﻿using External.ServiceModel;
+using ServiceStack;
+
+namespace Test.ServiceInterface
+{
+    public class ExternalServices : Service
+    {
+        public object Any(ExternalOperation request)
+        {
+            return new ExternalOperationResponse { Result = request.Name };
+        }
+
+        public object Any(ExternalOperation2 request)
+        {
+            return new ExternalOperation2Response
+            {
+                ExternalType = new ExternalType
+                {
+                    ExternalEnum2 = ExternalEnum2.Due
+                }
+            };
+        }
+
+        public ExternalReturnTypeResponse Any(ExternalOperation3 request)
+        {
+            return new ExternalReturnTypeResponse
+            {
+                ExternalEnum3 = ExternalEnum3.Deux
+            };
+        }
+
+        public object Any(ExternalOperation4 request)
+        {
+            return new ExternalUnmatchedResponse
+            {
+                ExternalEnum4 = ExternalEnum4.Dos
+            };
+        }
+    }
+}
