8cccf61bd82b92cef8305a4f770e7813d7005f5e
Demis Bellot <demis.bellot@gmail.com>
2015-02-19 10:19:09 -0500

    Add test services for null and void responses

---
+++ src/Test/Test.ServiceInterface/TestServices.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack;
+
+namespace Test.ServiceInterface
+{
+    [Route("/void-response")]
+    public class TestVoidResponse { }
+
+    [Route("/null-response")]
+    public class TestNullResponse { }
+
+    public class TestServices : Service
+    {
+        public void Any(TestVoidResponse response)
+        {
+        }
+
+        public object Any(TestNullResponse response)
+        {
+            return null;
+        }
+    }
+}
