94c34b8997d8a610de602bf707a2181ae121163a
Demis Bellot <demis.bellot@gmail.com>
2016-10-30 00:32:41 -0400

    Add jQuery.d.ts via typings

--- src/Test/Test/typings/index.d.ts
+++ src/Test/Test/typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/isomorphic-fetch/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/ss-utils/index.d.ts" />
 /// <reference path="modules/servicestack-client/index.d.ts" />
