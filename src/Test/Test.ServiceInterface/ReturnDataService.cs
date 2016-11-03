ad3469ee1cb99074c5e2e3828521bd32d101ac1f
Demis Bellot <demis.bellot@gmail.com>
2016-11-03 03:45:16 -0400

    Add Return Data Services

---
+++ src/Test/Test.ServiceInterface/ReturnDataService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using Test.ServiceModel;
+
+namespace Test.ServiceInterface
+{
+    public class ReturnDataService : Service
+    {
+        public object Any(ReturnString request) => request.Data;
+        public object Any(ReturnBytes request) => request.Data;
+        public object Any(ReturnStream request) => request.Data;
+        public object Any(ReturnHttpWebResponse request) => request.Data;
+    }
+}
