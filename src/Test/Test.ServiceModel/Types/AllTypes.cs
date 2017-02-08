47672c6d532e493552fc0c83166b5284a979b24d
Demis Bellot <demis.bellot@gmail.com>
2017-02-07 23:02:38 -0500

    Update SS dlls

--- src/Test/Test.ServiceModel/Types/AllTypes.cs
+++ src/Test/Test.ServiceModel/Types/AllTypes.cs
@@ -23,6 +23,7 @@ namespace Test.ServiceModel.Types
         public DateTimeOffset DateTimeOffset { get; set; }
         public Guid Guid { get; set; }
         public Char Char { get; set; }
+        public KeyValuePair<string, string> KeyValuePair { get; set; }
         public DateTime? NullableDateTime { get; set; }
         public TimeSpan? NullableTimeSpan { get; set; }
         public List<string> StringList { get; set; }
