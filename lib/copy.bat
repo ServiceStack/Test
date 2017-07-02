dcd464bd014ceab7632fc0351a55de59b24c057a
Demis Bellot <demis.bellot@gmail.com>
2017-07-01 22:17:05 -0400

    Update to latest SS dlls

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,16 +1,11 @@
 SET BUILD=Release
 REM SET BUILD=DEBUG
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\ServiceStack.OrmLite.MySql.* .
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\MySql.Data.* .
+COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.MySql\bin\%BUILD%\net45\ServiceStack.OrmLite.MySql.* .
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.* .
+COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\net45\ServiceStack.OrmLite.SqlServer.* .
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\ServiceStack.OrmLite.Sqlite.* .
-COPY C:\src\ServiceStack\lib\Mono.Data.Sqlite.dll .
-COPY C:\src\ServiceStack\lib\sqlite3.dll .
-
-COPY C:\src\ServiceStack.Redis\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.* .
+COPY C:\src\ServiceStack.Redis\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.* .
 
 COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\%BUILD%\ServiceStack.Authentication.OAuth2.* .
 COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\%BUILD%\DotNetOpenAuth.* .
@@ -19,13 +14,11 @@ COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OpenId\bin\%BUILD%\Serv
 
 COPY C:\src\ServiceStack\src\ServiceStack.Logging.Log4Net\bin\%BUILD%\ServiceStack.Logging.Log4Net.* .
 
-COPY C:\src\ServiceStack\src\ServiceStack.Api.Swagger\bin\%BUILD%\ServiceStack.Api.Swagger.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\ServiceStack.Server.* .
-COPY C:\src\ServiceStack\src\ServiceStack.RabbitMq\bin\%BUILD%\ServiceStack.RabbitMq.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Mvc\bin\%BUILD%\ServiceStack.Mvc.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\%BUILD%\* .
+COPY C:\src\ServiceStack\src\ServiceStack.Api.OpenApi\bin\%BUILD%\net45\ServiceStack.Api.OpenApi.* .
+COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\net45\ServiceStack.Server.* .
+COPY C:\src\ServiceStack\src\ServiceStack.RabbitMq\bin\%BUILD%\net45\ServiceStack.RabbitMq.* .
+COPY C:\src\ServiceStack\src\ServiceStack.Mvc\bin\%BUILD%\net45\ServiceStack.Mvc.* .
+COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\%BUILD%\net45\* .
 
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSqlV45\bin\%BUILD%\ServiceStack.OrmLite.* .
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSqlV45\bin\%BUILD%\Mono.Security.dll .
-COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSqlV45\bin\%BUILD%\Npgsql.dll .
+COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.PostgreSql\bin\%BUILD%\net45\ServiceStack.OrmLite.* .
 
